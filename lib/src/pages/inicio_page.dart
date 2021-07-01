95bdfd0acc298c9686eb1b74228e524a1ffc6840
Eduardo Olivo <keolivor@utn.edu.ec>
2021-07-01 12:23:26 -0500

    Se creo widget opciones para la página de inicio

---
+++ lib/src/pages/inicio_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class InicioPage extends StatelessWidget {
+  const InicioPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ejemplos bloc'),
+      ),
+      body: _opciones(context),
+    );
+
+  }
+
+  Widget _opciones(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        _suma(context)
+      ],
+    );
+  }
+
+  Widget _suma(BuildContext context) {
+    return ListTile(
+      title: Text('Suma de dos números'),
+      subtitle: Text('Utilizando bloc'),
+      onTap: () {
+        Navigator.pushNamed(context, '/suma');
+      },
+    );
+  }
+}
