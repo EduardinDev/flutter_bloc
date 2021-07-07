ec4967c3a3795be70b243223278c30e20ecdb15c
Eduardo Olivo <keolivor@utn.edu.ec>
2021-07-06 22:58:18 -0500

    Se borraron comentarios innecesarios

--- lib/src/bloc/resta_bloc.dart
+++ lib/src/bloc/resta_bloc.dart
@@ -2,39 +2,21 @@ import 'package:rxdart/rxdart.dart';
 
 class RestaBloc {
   RestaBloc() {
-    changeNumero1('0');
-    changeNumero2('0');
+     numero1Controller.sink.add('0');
+     numero2Controller.sink.add('0');
   }
 
   // controladores
-  final numero1Controller = BehaviorSubject<String>();
-  final numero2Controller = BehaviorSubject<String>();
+  final numero1Controller   = BehaviorSubject<String>();
+  final numero2Controller   = BehaviorSubject<String>();
   final resultadoController = BehaviorSubject<String>();
 
   // streams, son los flujos de información
 
-  Stream<String> get numero1Stream => numero1Controller.stream;
-  Stream<String> get numero2Stream => numero2Controller.stream;
+  Stream<String> get numero1Stream   => numero1Controller.stream;
+  Stream<String> get numero2Stream   => numero2Controller.stream;
   Stream<String> get resultadoStream => resultadoController.stream;
 
-  // insersores
-
-  Function(String) get changeNumero1 => numero1Controller.sink.add;
-  Function(String) get changeNumero2 => numero2Controller.sink.add;
-  Function(String) get changeResultado => resultadoController.sink.add;
-
-  // obtener ultimo valor ingresado en el stream
-
-  String get numero1 => numero1Controller.value;
-  String get numero2 => numero2Controller.value;
-  String get resultado => resultadoController.value;
-
-  // operacion resta
-
- /*  operacionResta() {
-    int resta = int.parse(numero1) - int.parse(numero2);
-    changeResultado(resta.toString());
-  } */
 
   dispose() {
     numero1Controller.close();
--- lib/src/bloc/suma_bloc.dart
+++ lib/src/bloc/suma_bloc.dart
@@ -6,36 +6,21 @@ class SumaBloc {
 
 // al empezar la aplicación se inicializan los numeros con cero
 SumaBloc(){
-  changeNumero1('0');
-  changeNumero2('0');
+  numero1Controller.sink.add('0');
+  numero2Controller.sink.add('0');
 }
 
   // los controladores de los stream
-  final numero1Controller = BehaviorSubject<String>();
-  final numero2Controller = BehaviorSubject<String>();
+  final numero1Controller   = BehaviorSubject<String>();
+  final numero2Controller   = BehaviorSubject<String>();
   final resultadoController = BehaviorSubject<String>();
 
   // los streams de los controladores
-  Stream<String> get numero1Stream => numero1Controller.stream;
-  Stream<String> get numero2Stream => numero2Controller.stream;
+  Stream<String> get numero1Stream   => numero1Controller.stream;
+  Stream<String> get numero2Stream   => numero2Controller.stream;
   Stream<String> get resultadoStream => resultadoController.stream;
 
-  // Insersores de valores a los streams
-  Function(String) get changeNumero1 => numero1Controller.sink.add;
-  Function(String) get changeNumero2 => numero2Controller.sink.add;
-  Function(String) get changeResultado => resultadoController.sink.add;
-
-  // obtener ultimos valores de los streams
-  String get numero1 => numero1Controller.value;
-  String get numero2 => numero2Controller.value;
-  String get resultado => resultadoController.value;
-
-  // sumar valores
-  sumarNumeros() {
-    int operacion = int.parse(numero1) + int.parse(numero2);
-    changeResultado(operacion.toString());
-  }
-
+ 
   // cerrar los streams
   // nota: si no se cierran entonces volveran a crearse
   // cada vez que se inicie la clase en donde se los instancio
